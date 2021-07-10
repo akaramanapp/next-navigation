68cd5e31d7d68f38f14947ccf381c5050181f127
Abdulkerim Karaman <abdulkerim.karamna@albarakatech.com>
2021-07-10 20:32:11 +0300

    add config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://next-navigation-five.vercel.app/:path*',
+      },
+    ]
+  },
 }
